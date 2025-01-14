1a2f4d24f26657026a54df7948c872ec14535df6
Deepak Kumar Majhi <155293118+deepak443mki@users.noreply.github.com>
2025-01-14 19:21:57 +0530

    Add files via upload

---
+++ IPFinder.java
@@ -0,0 +1,18 @@
+import java.net.*;
+import java.util.*;
+
+public class IPFinder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String host;
+        InetAddress address;
+        System.out.println("\n Enter Host Name: ");
+        host = input.nextLine();
+        try{
+            address = InetAddress.getByName(host);
+            System.out.println("IP Address - "+address);
+        } catch (UnknownHostException e) {
+            System.out.println("Could not found"+host);
+        }
+    }
+}
